dcb6cf2a29f38aefbbe383e38d532837186a9d99
dago83 <85219759+dago83@users.noreply.github.com>
2025-11-18 17:27:44 +0300

    commit2

--- config/db.js
+++ config/db.js
@@ -2,18 +2,18 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false 
+  }
 });
 
 
 pool.query('SELECT NOW()', (err, res) => {
   if (err) {
-    console.error(' Database connection error:', err.stack);
+    console.error('PostgreSQL connection failed:', err.stack);
   } else {
     console.log(' Connected to PostgreSQL');
   }
